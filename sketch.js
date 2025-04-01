fede917664aca155261a6e15b628e2d86066f9b3
Wang-Simona <wyingxuan9@gmail.com>
2025-04-01 10:15:00 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,3 +1,8 @@
+let font;
+function preload() {
+  font = loadFont("LXGWWenKai-Light.ttf");
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
@@ -6,10 +11,17 @@ function draw() {
   background(220);
   const s = 100;
 
+  fill(127, 255, 0);
   rect(0, 0, s, s);
   rect(0, height - s, s, s);
   rect(width - s, 0, s, s);
   rect(width - s, height - s, s, s);
+
+  fill(255, 127, 80);
+  textSize(100);
+  textAlign(CENTER);
+  textFont(font);
+  text("wang yingxuan", windowWidth / 2, windowHeight / 2);
 }
 
 function windowResized() {
